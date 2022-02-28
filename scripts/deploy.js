e7c801e77f8669c1eb2b1265629c26738f0a7918
guorong <dudurong009@gmail.com>
2022-02-27 19:27:06 -0700

    Fix the "deploy.js" script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,8 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-  
     const customNFT = await ethers.getContractFactory("CustomNFT");
     const cNFT = await customNFT.deploy();
-  
     console.log("CustomNFT address:", cNFT.address);
   }
   
